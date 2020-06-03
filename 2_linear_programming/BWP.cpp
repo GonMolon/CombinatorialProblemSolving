74b0862c1dc6e05fb212ea3ed9e36b46be2f726b
Gonzalo Solera <gonzalo1796@gmail.com>
2020-06-03 20:22:51 +0200

    Implemented lienar programming model with two variables for position. Weird error since some found solutions don't satisfy overlapping constraintsgit status

--- 2_linear_programming/BWP.cpp
+++ 2_linear_programming/BWP.cpp
@@ -6,6 +6,9 @@ ILOSTLBEGIN
 #include "../include/BWPInstance.hh"
 
 
+#define width(i) (dir[i] * instance.boxes[i].first + (1 - dir[i]) * instance.boxes[i].second)
+#define height(i) (dir[i] * instance.boxes[i].second + (1 - dir[i]) * instance.boxes[i].first)
+
 IloNumVar L;
 IloNumVarArray pos_x;
 IloNumVarArray pos_y;
@@ -14,52 +17,57 @@ IloBoolVarArray dir;
 IloModel create_model(IloEnv env, BWPInstance instance) {
     IloModel model = IloModel(env);
 
+    L = IloNumVar(env, instance.L_lower_bound, instance.L_upper_bound);
     pos_x = IloNumVarArray(env, instance.N);
     pos_y = IloNumVarArray(env, instance.N);
     dir = IloBoolVarArray(env, instance.N);
 
-    // int aux_bool_var_count = instance.N * (instance.N - 1) / 2;
-    // overlap_top = IloBoolVarArray(env, aux_bool_var_count);
-    // overlap_bottom = IloBoolVarArray(env, aux_bool_var_count);
-    // overlap_left = IloBoolVarArray(env, aux_bool_var_count);
-    // overlap_right = IloBoolVarArray(env, aux_bool_var_count);
-
-    vector<IloNumExprArg> width = vector<IloNumExprArg>(instance.N);
-    vector<IloNumExprArg> height = vector<IloNumExprArg>(instance.N);
-
-    double precision = double(instance.W - 1) / instance.W - (instance.W - 1) / instance.W;
-    cerr << "Precision for floor operation: " << precision << endl;
     for(int i = 0; i < instance.N; ++i) {
-        width[i] = dir[i] * instance.boxes[i].first + (1 - dir[i]) * instance.boxes[i].second;
-        height[i] = dir[i] * instance.boxes[i].second + (1 - dir[i]) * instance.boxes[i].first;
-
-        pos[i] = IloNumVar(env, 0, instance.W * (instance.L_upper_bound - instance.boxes[i].first + 1) - 1);
+        pos_x[i] = IloNumVar(env, 0, instance.W - instance.boxes[i].first);
+        pos_y[i] = IloNumVar(env, 0, instance.L_upper_bound - instance.boxes[i].first);
+        model.add(pos_x[i] <= instance.W - width(i));
         if(instance.boxes[i].first == instance.boxes[i].second || instance.boxes[i].second > instance.W) {
             model.add(dir[i]);
         }
     }
 
-    int index = 0;
     for(int i = 0; i < instance.N; ++i) {
-        // model.add(L >= pos[i] / instance.W + height[i]);
+        model.add(L >= pos_y[i] + height(i));
 
-        // for(int j = i + 1; j < instance.N; ++j) {
-
-        //     BoolVar top(overlap_top[index]);
-        //     BoolVar bottom(overlap_bottom[index]);
-        //     BoolVar left(overlap_left[index]);
-        //     BoolVar right(overlap_right[index]);
-        //     ++index;
-
-        //     rel(*this, pos[j] % instance.W + right * instance.W                 >= pos[i] % instance.W + width[i]);
-        //     rel(*this, pos[j] % instance.W + width[j]                           <= pos[i] % instance.W + left * instance.W);
-        //     rel(*this, pos[j] / instance.W + bottom * instance.L_upper_bound    >= pos[i] / instance.W + height[i]);
-        //     rel(*this, pos[j] / instance.W + height[j]                          <= pos[i] / instance.W + top * instance.L_upper_bound);
-        //     rel(*this, top + bottom + left + right <= 3);
-        // }
+        for(int j = i + 1; j < instance.N; ++j) {
+            model.add(
+                pos_x[j] >= pos_x[i] + width(i) || 
+                pos_x[j] + width(j) <= pos_x[i] || 
+                pos_y[j] >= pos_y[i] + height(i) || 
+                pos_y[j] + height(j) <= pos_y[i]
+            );
+        }
     }
 
+    model.add(IloMinimize(env, L));
+
     return model;
+}
+
+
+void print_solution(IloCplex cplex, BWPInstance instance) {
+    cout << cplex.getObjValue() << endl;
+    for(int i = 0; i < instance.N; ++i) {
+        int x = cplex.getValue(pos_x[i]);
+        int y = cplex.getValue(pos_y[i]);
+        bool d = cplex.getValue(dir[i]);
+        cout << x << " " << y << " " 
+        << x + d * instance.boxes[i].first + (1 - d) * instance.boxes[i].second - 1 << " " 
+        << y + d * instance.boxes[i].second + (1 - d) * instance.boxes[i].first - 1 << endl;
+    }
+    for(int i = 0; i < instance.N; ++i) {
+        for(int j = 0; j < instance.N; ++j) {
+            if(i != j) {
+                cerr << i << ", " << j << endl;
+                cerr << cplex.getValue(pos_x[j] >= pos_x[i] + width(i)|| pos_x[j] + width(j) <= pos_x[i] || pos_y[j] >= pos_y[i] + height(i) || pos_y[j] + height(j) <= pos_y[i]) << endl;
+            }
+        }
+    }
 }
 
 
@@ -70,9 +78,9 @@ int main() {
     IloEnv env;
     IloModel model = create_model(env, instance);
 
-    return 0;
     IloCplex cplex(model);
+    cplex.setOut(env.getNullStream());
     cplex.solve();
-    cout << cplex.getObjValue() << endl;
+    print_solution(cplex, instance);
     env.end();    
 }
